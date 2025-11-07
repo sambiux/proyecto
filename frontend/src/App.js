e0556cc39f4a488e4806fc69ce2b1bb4eb330ac6
sambiux <sddaza.9@gmail.com>
2025-11-07 14:09:12 +0000

    Implementando el nuevo componente

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import Usuarios from "./components/usuarios";
+import LoginYUsuarios from "./LoginYUsuarios";
 
 function App() {
   return (
     <div>
-      <Usuarios />
+      <LoginYUsuarios/>
     </div>
   );
 }
